d3eed6ce682d3bb2b11a99309f1f526edd4be61f
adnsyhh <147686674+adnsyhh@users.noreply.github.com>
2024-10-24 16:07:54 +0700

    Update DokterHewan.java

--- src/main/java/com/mycompany/forum5/DokterHewan.java
+++ src/main/java/com/mycompany/forum5/DokterHewan.java
@@ -1,1 +1,20 @@
+public class DokterHewan {
+  private String nama;
+  private String spesialis
 
+  public DokterHewan(String nama, String spesialis) {
+    this.nama = nama;
+    this.spesialis = spesialis;
+  }
+
+  public String getNama() {
+    return this.nama;
+  }
+  public String getSpesialis() {
+    return this.spesialis;
+  }
+
+  public void periksaHewan(Hewan hewan) {
+    System.out.println("Dokter ini " + nama + " memeriksa hewan " + hewan.nama);
+  }
+}
